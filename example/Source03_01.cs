13765a3346b3dc7225e2be2681256a8e6247d500
yulian <yulian@naver.com>
2022-09-24 16:34:54 +0900

    Create Source03_01.cs
    Do it! 실습 - 변수에 저장된 값 출력하기

---
+++ example/Source03_01.cs
@@ -0,0 +1,19 @@
+using System;
+
+class MainClass
+{
+  public static void Main(string[] args)
+  {
+    // 변수 선언과 초기화
+    bool boolVariable = true;
+    int intVariable = 10;
+    float floatVariable = 3.4f;
+    char charVariable = 'a';
+    
+    // 변수에 저장된 값을 콘솔에 출력
+    Console.WriteLine(boolVariable);
+    Console.WriteLine(intVariable);
+    Console.WriteLine(floatVariable);
+    Console.WriteLine(charVariable);
+  }
+}
