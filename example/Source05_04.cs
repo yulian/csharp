146be8b7d7352e1446f396d49866101df78dae05
yulian <yulian@naver.com>
2022-10-12 22:27:51 +0900

    Update Source05_04.cs
    코드 가독성 개선

--- example/Source05_04.cs
+++ example/Source05_04.cs
@@ -1,1 +1,13 @@
-using System;class MainClass {  public static void Main(string[] args) {    // 배열을 초기화하는 세 번째 방법    int[] array3 = { 4, 5, 6 };        // foreach 문으로 배열에 담긴 값 출력    foreach(int item in array3) {      Console.WriteLine(item);    }  }}
+using System;
+
+class MainClass {
+  public static void Main(string[] args) {
+    // 배열을 초기화하는 세 번째 방법
+    int[] array3 = { 4, 5, 6 };
+    
+    // foreach 문으로 배열에 담긴 값 출력
+    foreach(int item in array3) {
+      Console.WriteLine(item);
+    }
+  }
+}
