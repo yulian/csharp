d11075abcf3c32e2be93f7219d80bbaceaf5bd86
yulian <yulian@naver.com>
2022-10-12 22:31:44 +0900

    Update Source05_11.cs
    코드 가독성 개선

--- example/Source05_11.cs
+++ example/Source05_11.cs
@@ -1,1 +1,12 @@
-using System;class MainClass {  public static void Main(string[] args) {    // ReadLine() 메서드로 사용자 입력 받기    Console.Write("나눌 숫자를 입력하세요 : ");    int divider = int.Parse(Console.ReadLine());        // 숫자 10을 사용자 입력한 숫자로 나눈 몫을 출력    Console.WriteLine(10 / divider);  }}
+using System;
+
+class MainClass {
+  public static void Main(string[] args) {
+    // ReadLine() 메서드로 사용자 입력 받기
+    Console.Write("나눌 숫자를 입력하세요 : ");
+    int divider = int.Parse(Console.ReadLine());
+    
+    // 숫자 10을 사용자 입력한 숫자로 나눈 몫을 출력
+    Console.WriteLine(10 / divider);
+  }
+}
