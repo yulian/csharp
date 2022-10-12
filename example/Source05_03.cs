56d9d1fd77f1ad225e120af0dd6a94a32ee3824c
yulian <yulian@naver.com>
2022-10-12 22:21:38 +0900

    Update Source05_03.cs
    코드 가독성 개선

--- example/Source05_03.cs
+++ example/Source05_03.cs
@@ -1,1 +1,13 @@
-using System;class MainClass {  public static void Main(string[] args) {    // 배열을 초기화하는 두 번째 방법    int[] array2 = new int[] { 1, 2, 3 };        // for 반복문으로 배열의 인덱스 접근과 값 출력    for (int i = 0; i < 3; i++) {      Console.WriteLine(array2[i]);    }  }}
+using System;
+
+class MainClass {
+  public static void Main(string[] args) {
+    // 배열을 초기화하는 두 번째 방법
+    int[] array2 = new int[] { 1, 2, 3 };
+    
+    // for 반복문으로 배열의 인덱스 접근과 값 출력
+    for (int i = 0; i < 3; i++) {
+      Console.WriteLine(array2[i]);
+    }
+  }
+}
