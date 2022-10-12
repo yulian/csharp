87b08f76d723bb903cedd510084d99af16f07ac7
yulian <yulian@naver.com>
2022-10-12 22:29:53 +0900

    Update Source05_08.cs
    코드 가독성 개선

--- example/Source05_08.cs
+++ example/Source05_08.cs
@@ -1,1 +1,18 @@
-using System;using System.Collections;class MainClass {  public static void Main(string[] args) {    Queue qu = new Queue();        // Enqueue() 메서드로 큐에 아이템 추가    qu.Enqueue(1);    qu.Enqueue(2);    qu.Enqueue(3);        // Dequeue() 메서드로 큐에서 아이템 제거    while (qu.Count > 0) {      Console.WriteLine(qu.Dequeue());    }  }}
+using System;
+using System.Collections;
+
+class MainClass {
+  public static void Main(string[] args) {
+    Queue qu = new Queue();
+    
+    // Enqueue() 메서드로 큐에 아이템 추가
+    qu.Enqueue(1);
+    qu.Enqueue(2);
+    qu.Enqueue(3);
+    
+    // Dequeue() 메서드로 큐에서 아이템 제거
+    while (qu.Count > 0) {
+      Console.WriteLine(qu.Dequeue());
+    }
+  }
+}
