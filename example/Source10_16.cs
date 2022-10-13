6dcbb64ab3b772ed3e4bfd3eda6af42eb019904d
yulian <yulian@naver.com>
2022-10-14 02:25:43 +0900

    Update Source10_16.cs
    코드 가독성 개선

--- example/Source10_16.cs
+++ example/Source10_16.cs
@@ -1,1 +1,29 @@
-using System;class Person {  private string name;  private int age;  private string gender;    public Person(string name, int age, string gender) {    this.name = name;    this.age = age;    this.gender = gender;  }    public void PrintPerson() {    Console.WriteLine("이름 : " + this.name);    Console.WriteLine("나이 : " + this.age);    Console.WriteLine("성별 : " + this.gender);  }}class MainClass {  public static void Main(string[] args) {    Person y = new Person("영희", 20, "여");    y.PrintPerson();        Person c = new Person("남", 20, "철수");    c.PrintPerson();  }}
+using System;
+
+class Person {
+  private string name;
+  private int age;
+  private string gender;
+  
+  public Person(string name, int age, string gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+  
+  public void PrintPerson() {
+    Console.WriteLine("이름 : " + this.name);
+    Console.WriteLine("나이 : " + this.age);
+    Console.WriteLine("성별 : " + this.gender);
+  }
+}
+
+class MainClass {
+  public static void Main(string[] args) {
+    Person y = new Person("영희", 20, "여");
+    y.PrintPerson();
+    
+    Person c = new Person("남", 20, "철수");
+    c.PrintPerson();
+  }
+}
