ed832b90535d4abc52c5e966af78de5380e41702
yulian <yulian@naver.com>
2022-10-14 01:56:29 +0900

    Update Source08_03.cs
    코드 가독성 개선

--- example/Source08_03.cs
+++ example/Source08_03.cs
@@ -1,1 +1,21 @@
-using System.Collections;using System.Collections.Generic;using UnityEngine;public class OnMouse: MonoBehaviour {  private void OnMouseEnter() {    print("마우스 커서가 들어왔습니다");  }    private void OnMouseExit() {    print("마우스 커서가 나갔습니다");  }    private void OnMouseDown() {    print("마우스 버튼을 누릅니다");  }    private void OnMouseUp() {    print("마우스 버튼을 떼었습니다");  }}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouse: MonoBehaviour {
+  private void OnMouseEnter() {
+    print("마우스 커서가 들어왔습니다");
+  }
+  
+  private void OnMouseExit() {
+    print("마우스 커서가 나갔습니다");
+  }
+  
+  private void OnMouseDown() {
+    print("마우스 버튼을 누릅니다");
+  }
+  
+  private void OnMouseUp() {
+    print("마우스 버튼을 떼었습니다");
+  }
+}
