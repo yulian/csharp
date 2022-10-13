3ca1c79209f244f60b60d422cdb064835007f50c
yulian <yulian@naver.com>
2022-10-14 01:57:54 +0900

    Update Source08_08.cs
    코드 가독성 개선

--- example/Source08_08.cs
+++ example/Source08_08.cs
@@ -1,1 +1,16 @@
-using System.Collections;using System.Collections.Generic;using UnityEngine;public class Collision: MonoBehaviour {  public AudioSource collisionSound;    void Start() {    collisionSound = GetComponent < AudioSource > ();  }    private void OnCollisionEnter(UnityEngine.Collision collision) {    collisionSound.Play();    print("충돌이 발생했습니다");  }}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision: MonoBehaviour {
+  public AudioSource collisionSound;
+  
+  void Start() {
+    collisionSound = GetComponent < AudioSource > ();
+  }
+  
+  private void OnCollisionEnter(UnityEngine.Collision collision) {
+    collisionSound.Play();
+    print("충돌이 발생했습니다");
+  }
+}
