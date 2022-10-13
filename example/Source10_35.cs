1a06565dcdb6fe59a8b0a1bfe562eae0e082c911
yulian <yulian@naver.com>
2022-10-14 02:30:39 +0900

    Update Source10_35.cs
    코드 가독성 개선

--- example/Source10_35.cs
+++ example/Source10_35.cs
@@ -1,1 +1,9 @@
-using System;class MainClass {  public static void Main(string[] args) {    var t = ("우체국", 3.6);    (string destination, double distance) = t;    Console.WriteLine($"{destination}까지 거리는 {distance}킬로미터입니다.");  }}
+using System;
+
+class MainClass {
+  public static void Main(string[] args) {
+    var t = ("우체국", 3.6);
+    (string destination, double distance) = t;
+    Console.WriteLine($"{destination}까지 거리는 {distance}킬로미터입니다.");
+  }
+}
