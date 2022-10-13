2dbc3e9e6801085436a23ebccf13b65060170125
yulian <yulian@naver.com>
2022-10-14 01:57:07 +0900

    Update Source08_05.cs
    코드 가독성 개선

--- example/Source08_05.cs
+++ example/Source08_05.cs
@@ -1,1 +1,15 @@
-using System.Collections;using System.Collections.Generic;using UnityEngine;public class MoveObject: MonoBehaviour {  float speed = 20 F;    void Start() {}    void Update() {    float position = Input.GetAxis("Vertical");    position = position * speed * Time.deltaTime;    transform.Translate(Vector3.forward * position);  }}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject: MonoBehaviour {
+  float speed = 20 F;
+  
+  void Start() {}
+  
+  void Update() {
+    float position = Input.GetAxis("Vertical");
+    position = position * speed * Time.deltaTime;
+    transform.Translate(Vector3.forward * position);
+  }
+}
