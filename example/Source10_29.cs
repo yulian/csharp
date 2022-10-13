faae82126af5aecd557884fec523569bec679b5a
yulian <yulian@naver.com>
2022-10-14 02:29:10 +0900

    Update Source10_29.cs
    코드 가독성 개선

--- example/Source10_29.cs
+++ example/Source10_29.cs
@@ -1,1 +1,21 @@
-using System;public class Person {  public string Name {    get => name;    set {      if (string.IsNullOrWhiteSpace(value))        throw new ArgumentException("Name must not be blank");      name = value;    }  }  private string name;}class MainClass {  public static void Main(string[] args) {    Person p = new Person();    p.Name = "";    Console.WriteLine(p.Name);  }}
+using System;
+
+public class Person {
+  public string Name {
+    get => name;
+    set {
+      if (string.IsNullOrWhiteSpace(value))
+        throw new ArgumentException("Name must not be blank");
+      name = value;
+    }
+  }
+  private string name;
+}
+
+class MainClass {
+  public static void Main(string[] args) {
+    Person p = new Person();
+    p.Name = "";
+    Console.WriteLine(p.Name);
+  }
+}
