49171b7b8bbcddc5d2334bfecb5467b6363d49ff
yulian <yulian@naver.com>
2022-10-14 02:23:20 +0900

    Update Source10_07.cs
    코드 가독성 개선

--- example/Source10_07.cs
+++ example/Source10_07.cs
@@ -1,1 +1,33 @@
-using System;class Human {  private string name;  private int age;    public void SetName(string name) {    this.name = name;  }    public string GetName() {    return this.name;  }    public void SetAge(int age) {    this.age = age;  }    public int GetAge() {    return this.age;  }}class MainClass {  public static void Main(string[] args) {    Human h1 = new Human();    h1.SetName("미솔");    h1.SetAge(7);        Console.WriteLine(h1.GetName());    Console.WriteLine(h1.GetAge());  }}
+using System;
+
+class Human {
+  private string name;
+  private int age;
+  
+  public void SetName(string name) {
+    this.name = name;
+  }
+  
+  public string GetName() {
+    return this.name;
+  }
+  
+  public void SetAge(int age) {
+    this.age = age;
+  }
+  
+  public int GetAge() {
+    return this.age;
+  }
+}
+
+class MainClass {
+  public static void Main(string[] args) {
+    Human h1 = new Human();
+    h1.SetName("미솔");
+    h1.SetAge(7);
+    
+    Console.WriteLine(h1.GetName());
+    Console.WriteLine(h1.GetAge());
+  }
+}
