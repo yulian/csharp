f11dc76a46e8e27affb4b1b119bd7272638e9780
yulian <yulian@naver.com>
2022-10-14 02:22:31 +0900

    Update Source10_04.cs
    코드 가독성 개선

--- example/Source10_04.cs
+++ example/Source10_04.cs
@@ -1,1 +1,38 @@
-using System;class GenericClass < T > {  private T[] values;  private int count;    public GenericClass(int length) {    values = new T[length];    count = length;  }    public void SetValue(T item, int index) {    this.values[index] = item;  }    public void PrintValue() {    foreach(T item in values) {      Console.WriteLine(item);    }  }}class MainClass {  public static void Main(string[] args) {    GenericClass < int > intArray = new GenericClass < int > (3);    GenericClass < double > doubleArray = new GenericClass < double > (3);        intArray.SetValue(1, 0);    intArray.SetValue(2, 1);    intArray.SetValue(3, 2);    intArray.PrintValue();        doubleArray.SetValue(1.1, 0);    doubleArray.SetValue(2.2, 1);    doubleArray.SetValue(3.3, 2);    doubleArray.PrintValue();  }}
+using System;
+
+class GenericClass < T > {
+  private T[] values;
+  private int count;
+  
+  public GenericClass(int length) {
+    values = new T[length];
+    count = length;
+  }
+  
+  public void SetValue(T item, int index) {
+    this.values[index] = item;
+  }
+  
+  public void PrintValue() {
+    foreach(T item in values) {
+      Console.WriteLine(item);
+    }
+  }
+}
+
+class MainClass {
+  public static void Main(string[] args) {
+    GenericClass < int > intArray = new GenericClass < int > (3);
+    GenericClass < double > doubleArray = new GenericClass < double > (3);
+    
+    intArray.SetValue(1, 0);
+    intArray.SetValue(2, 1);
+    intArray.SetValue(3, 2);
+    intArray.PrintValue();
+    
+    doubleArray.SetValue(1.1, 0);
+    doubleArray.SetValue(2.2, 1);
+    doubleArray.SetValue(3.3, 2);
+    doubleArray.PrintValue();
+  }
+}
