43f0bc41e210785932f000cefd107fe7ca09ad61
yulian <yulian@naver.com>
2022-10-14 01:48:54 +0900

    Update Source06_01.cs
    코드 가독성 개선

--- example/Source06_01.cs
+++ example/Source06_01.cs
@@ -1,1 +1,16 @@
-using System;using System.Windows.Forms;namespace WindowsFormsApp1 {  public partial class Form1: Form {    public Form1() {      InitializeComponent();    }        private void Form1_Load(object sender, EventArgs e) {}        private void button1_Click(object sender, EventArgs e) {      label1.Text = "Hello World!!";    }  }}
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+  public partial class Form1: Form {
+    public Form1() {
+      InitializeComponent();
+    }
+    
+    private void Form1_Load(object sender, EventArgs e) {}
+    
+    private void button1_Click(object sender, EventArgs e) {
+      label1.Text = "Hello World!!";
+    }
+  }
+}
