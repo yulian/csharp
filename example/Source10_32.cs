9ceca696f42d7f16842de0e847581a4a9e51bd73
yulian <yulian@naver.com>
2022-10-14 02:29:53 +0900

    Update Source10_32.cs
    코드 가독성 개선

--- example/Source10_32.cs
+++ example/Source10_32.cs
@@ -1,1 +1,17 @@
-using System;class MainClass {  public static void Main(string[] args) {    int num1 = 3;    int num2 = 5;        // + 연산자를 활용한 문자열 연산    Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));        // String.Format() 메서드를 활용한 문자열 연산    Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);        // 문자열 보간을 활용한 문자열 연산    Console.WriteLine($"{num1} + {num2} = {num1+num2}");  }}
+using System;
+
+class MainClass {
+  public static void Main(string[] args) {
+    int num1 = 3;
+    int num2 = 5;
+    
+    // + 연산자를 활용한 문자열 연산
+    Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+    
+    // String.Format() 메서드를 활용한 문자열 연산
+    Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+    
+    // 문자열 보간을 활용한 문자열 연산
+    Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+  }
+}
