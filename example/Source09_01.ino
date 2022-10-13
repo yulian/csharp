b2832faefda347d044a1472b8eb81e8975b61ce8
yulian <yulian@naver.com>
2022-10-14 02:02:07 +0900

    Update Source09_01.ino
    코드 가독성 개선

--- example/Source09_01.ino
+++ example/Source09_01.ino
@@ -1,1 +1,15 @@
-void setup() {  Serial.begin(9600);  pinMode(13, OUTPUT);}void loop() {  if (Serial.available()) {    char ch = Serial.read();    if (ch == '1')      digitalWrite(13, HIGH);    else      digitalWrite(13, LOW);  }  delay(100);}
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()) {
+    char ch = Serial.read();
+    if (ch == '1')
+      digitalWrite(13, HIGH);
+    else
+      digitalWrite(13, LOW);
+  }
+  delay(100);
+}
