1f31aff17c98d9a2b21c59215627b7a5f12b1baf
yulian <yulian@naver.com>
2022-10-14 02:27:24 +0900

    Update Source10_22.cs
    코드 가독성 개선

--- example/Source10_22.cs
+++ example/Source10_22.cs
@@ -1,1 +1,18 @@
-using System;using System.Runtime.CompilerServices;class MainClass {  public static void Main(string[] args) {    TraceMessage("메서드 호출");  }    public static void TraceMessage(string message,    [CallerMemberName] string memberName = "",    [CallerFilePath] string sourceFilePath = "",    [CallerLineNumber] int sourceLineNumber = 0) {    Console.WriteLine("메시지: " + message);    Console.WriteLine("메서드 이름: " + memberName);    Console.WriteLine("파일의 경로: " + sourceFilePath);    Console.WriteLine("줄 번호: " + sourceLineNumber);  }}
+using System;
+using System.Runtime.CompilerServices;
+
+class MainClass {
+  public static void Main(string[] args) {
+    TraceMessage("메서드 호출");
+  }
+  
+  public static void TraceMessage(string message,
+    [CallerMemberName] string memberName = "",
+    [CallerFilePath] string sourceFilePath = "",
+    [CallerLineNumber] int sourceLineNumber = 0) {
+    Console.WriteLine("메시지: " + message);
+    Console.WriteLine("메서드 이름: " + memberName);
+    Console.WriteLine("파일의 경로: " + sourceFilePath);
+    Console.WriteLine("줄 번호: " + sourceLineNumber);
+  }
+}
