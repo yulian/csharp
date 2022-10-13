ffcf5e44f632328d835cb7bda3c200175f171858
yulian <yulian@naver.com>
2022-10-14 02:27:08 +0900

    Update Source10_21.cs
    코드 가독성 개선

--- example/Source10_21.cs
+++ example/Source10_21.cs
@@ -1,1 +1,19 @@
-using System;using System.Threading.Tasks;class MainClass {  public static async Task Main(string[] args) {    Task < string > t = AsyncFunc();    Console.WriteLine("1");    string value = await t;    Console.WriteLine("2");    Console.WriteLine(value);    Console.Read();  }    public static async Task < string > AsyncFunc() {    await Task.Delay(2000);    Console.WriteLine("3");    return "Hello";  }}
+using System;
+using System.Threading.Tasks;
+
+class MainClass {
+  public static async Task Main(string[] args) {
+    Task < string > t = AsyncFunc();
+    Console.WriteLine("1");
+    string value = await t;
+    Console.WriteLine("2");
+    Console.WriteLine(value);
+    Console.Read();
+  }
+  
+  public static async Task < string > AsyncFunc() {
+    await Task.Delay(2000);
+    Console.WriteLine("3");
+    return "Hello";
+  }
+}
