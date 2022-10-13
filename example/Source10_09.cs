c20e380b40e0a2880476ae71aea45c982b0cd3ae
yulian <yulian@naver.com>
2022-10-14 02:23:58 +0900

    Update Source10_09.cs
    코드 가독성 개선

--- example/Source10_09.cs
+++ example/Source10_09.cs
@@ -1,1 +1,10 @@
-using System;delegate int Add(int a, int b);class MainClass {  public static void Main(string[] args) {    Add add = (a, b) => a + b;    Console.WriteLine(add(1, 2));  }}
+using System;
+
+delegate int Add(int a, int b);
+
+class MainClass {
+  public static void Main(string[] args) {
+    Add add = (a, b) => a + b;
+    Console.WriteLine(add(1, 2));
+  }
+}
