d00a5bcf591a615845a4fa5ade8db3790540eee0
yulian <yulian@naver.com>
2022-10-14 02:21:41 +0900

    Update Source10_01.cs
    코드 가독성 개선

--- example/Source10_01.cs
+++ example/Source10_01.cs
@@ -1,1 +1,27 @@
-using System;delegate void myEventHandler();class Publisher {  public static event myEventHandler myEvent;  public void RunEvent() {    myEvent();  }}class Subscriber {  public Subscriber() {    Publisher.myEvent += TestMethod;  }  public void TestMethod() {    Console.WriteLine("이벤트 메서드 호출!!");  }}class MainClass {  public static void Main(string[] args) {    Publisher pub = new Publisher();    Subscriber sub = new Subscriber();    pub.RunEvent();  }}
+using System;
+
+delegate void myEventHandler();
+
+class Publisher {
+  public static event myEventHandler myEvent;
+  public void RunEvent() {
+    myEvent();
+  }
+}
+
+class Subscriber {
+  public Subscriber() {
+    Publisher.myEvent += TestMethod;
+  }
+  public void TestMethod() {
+    Console.WriteLine("이벤트 메서드 호출!!");
+  }
+}
+
+class MainClass {
+  public static void Main(string[] args) {
+    Publisher pub = new Publisher();
+    Subscriber sub = new Subscriber();
+    pub.RunEvent();
+  }
+}
