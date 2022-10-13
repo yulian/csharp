49e31d3d8cdbeabbc0198652ee098a5c0c35ff81
yulian <yulian@naver.com>
2022-10-14 02:32:14 +0900

    Update Source10_42.cs
    코드 가독성 개선

--- example/Source10_42.cs
+++ example/Source10_42.cs
@@ -1,1 +1,11 @@
-public class Circle {  public Circle(double radius) {    Radius = radius;  }    public double Radius { get; set; }    public void PrintDiameter() {    Console.WriteLine(2 * Radius);  }}
+public class Circle {
+  public Circle(double radius) {
+    Radius = radius;
+  }
+  
+  public double Radius { get; set; }
+  
+  public void PrintDiameter() {
+    Console.WriteLine(2 * Radius);
+  }
+}
