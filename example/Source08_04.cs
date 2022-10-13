c2a1a6422d0b4c511253e32e22a8c50aeb1edcb1
yulian <yulian@naver.com>
2022-10-14 01:56:50 +0900

    Update Source08_04.cs
    코드 가독성 개선

--- example/Source08_04.cs
+++ example/Source08_04.cs
@@ -1,1 +1,12 @@
-using System.Collections;using System.Collections.Generic;using UnityEngine;public class OnMouse: MonoBehaviour {  int count = 0;    private void OnMouseDown() {    count++;    print("Count: " + count);  }}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouse: MonoBehaviour {
+  int count = 0;
+  
+  private void OnMouseDown() {
+    count++;
+    print("Count: " + count);
+  }
+}
