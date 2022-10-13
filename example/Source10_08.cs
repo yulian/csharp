9aa90b6991ad6b722e52635863d215ec8d4bb912
yulian <yulian@naver.com>
2022-10-14 02:23:42 +0900

    Update Source10_08.cs
    코드 가독성 개선

--- example/Source10_08.cs
+++ example/Source10_08.cs
@@ -1,1 +1,17 @@
-using System;class Human {  public string name { get; set; }  public int age { get; set; }}class MainClass {  public static void Main(string[] args) {    Human h1 = new Human();    h1.name = "희솔";    h1.age = 8;        Console.WriteLine(h1.name);    Console.WriteLine(h1.age);  }}
+using System;
+
+class Human {
+  public string name { get; set; }
+  public int age { get; set; }
+}
+
+class MainClass {
+  public static void Main(string[] args) {
+    Human h1 = new Human();
+    h1.name = "희솔";
+    h1.age = 8;
+    
+    Console.WriteLine(h1.name);
+    Console.WriteLine(h1.age);
+  }
+}
