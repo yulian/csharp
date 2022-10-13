8175e22a70b0ca7483b6b7bc3238d884b074648c
yulian <yulian@naver.com>
2022-10-14 02:01:06 +0900

    Update Source09_02.cs
    코드 가독성 개선

--- example/Source09_02.cs
+++ example/Source09_02.cs
@@ -1,1 +1,25 @@
-using System;using System.IO.Ports;using System.Windows.Forms;namespace WindowsFormsApp {  public partial class Form1: Form {    SerialPort port = new SerialPort("COM3", 9600);        public Form1() {      InitializeComponent();    }        private void ButtonOn_Click(object sender, EventArgs e) {      port.Open();      port.Write("1");      port.Close();    }        private void ButtonOff_Click(object sender, EventArgs e) {      port.Open();      port.Write("0");      port.Close();    }  }}
+using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp {
+  public partial class Form1: Form {
+    SerialPort port = new SerialPort("COM3", 9600);
+    
+    public Form1() {
+      InitializeComponent();
+    }
+    
+    private void ButtonOn_Click(object sender, EventArgs e) {
+      port.Open();
+      port.Write("1");
+      port.Close();
+    }
+    
+    private void ButtonOff_Click(object sender, EventArgs e) {
+      port.Open();
+      port.Write("0");
+      port.Close();
+    }
+  }
+}
