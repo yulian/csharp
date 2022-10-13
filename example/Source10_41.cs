abc474c8922020bcf959a89f4593b786f79499fb
yulian <yulian@naver.com>
2022-10-14 02:32:01 +0900

    Update Source10_41.cs
    코드 가독성 개선

--- example/Source10_41.cs
+++ example/Source10_41.cs
@@ -1,1 +1,7 @@
-class MainClass {  public static void Main(string[] args) {    Console.WriteLine("Hello World!");    Circle c = new Circle(3);    c.PrintDiameter();  }}
+class MainClass {
+  public static void Main(string[] args) {
+    Console.WriteLine("Hello World!");
+    Circle c = new Circle(3);
+    c.PrintDiameter();
+  }
+}
