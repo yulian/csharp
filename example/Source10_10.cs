35bc8faa0ea965b68e14b54ab3a95164682150b8
yulian <yulian@naver.com>
2022-10-14 02:24:12 +0900

    Update Source10_10.cs
    코드 가독성 개선

--- example/Source10_10.cs
+++ example/Source10_10.cs
@@ -1,1 +1,8 @@
-using System;class MainClass {  public static void Main(string[] args) {    Func < int, int > square = x => x * x;    Console.WriteLine(square(5));  }}
+using System;
+
+class MainClass {
+  public static void Main(string[] args) {
+    Func < int, int > square = x => x * x;
+    Console.WriteLine(square(5));
+  }
+}
