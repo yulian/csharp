321a43d94e5037f627fcce445d31fcd59e0ca194
yulian <yulian@naver.com>
2022-10-14 02:28:55 +0900

    Update Source10_28.cs
    코드 가독성 개선

--- example/Source10_28.cs
+++ example/Source10_28.cs
@@ -1,1 +1,17 @@
-using System;public class Person {  public string Name {    get => name;    set => name = value;  }  private string name;}class MainClass {  public static void Main(string[] args) {    Person p = new Person();    p.Name = "홍길동";    Console.WriteLine(p.Name);  }}
+using System;
+
+public class Person {
+  public string Name {
+    get => name;
+    set => name = value;
+  }
+  private string name;
+}
+
+class MainClass {
+  public static void Main(string[] args) {
+    Person p = new Person();
+    p.Name = "홍길동";
+    Console.WriteLine(p.Name);
+  }
+}
