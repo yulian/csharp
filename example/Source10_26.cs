27a6f6d415868f90e35522ac8588cf857a8af520
yulian <yulian@naver.com>
2022-10-14 02:28:26 +0900

    Update Source10_26.cs
    코드 가독성 개선

--- example/Source10_26.cs
+++ example/Source10_26.cs
@@ -1,1 +1,13 @@
-using System;class Person {  public string name { get; } = "Default Name";}class MainClass {  public static void Main(string[] args) {    Person p = new Person();    p.name = "New Name";    Console.WriteLine(p.name);  }}
+using System;
+
+class Person {
+  public string name { get; } = "Default Name";
+}
+
+class MainClass {
+  public static void Main(string[] args) {
+    Person p = new Person();
+    p.name = "New Name";
+    Console.WriteLine(p.name);
+  }
+}
