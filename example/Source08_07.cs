66a9a107a7755c1051a2bc205697737e6afa4acb
yulian <yulian@naver.com>
2022-10-14 01:57:40 +0900

    Update Source08_07.cs
    코드 가독성 개선

--- example/Source08_07.cs
+++ example/Source08_07.cs
@@ -1,1 +1,9 @@
-using System.Collections;using System.Collections.Generic;using UnityEngine;public class Collision: MonoBehaviour {  private void OnCollisionEnter(UnityEngine.Collision collision) {    print("충돌이 발생했습니다");  }}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision: MonoBehaviour {
+  private void OnCollisionEnter(UnityEngine.Collision collision) {
+    print("충돌이 발생했습니다");
+  }
+}
