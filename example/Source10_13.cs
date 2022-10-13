cfa27d3c40cc58419b4bc51cfb70c3cdc7f59676
yulian <yulian@naver.com>
2022-10-14 02:24:55 +0900

    Update Source10_13.cs
    코드 가독성 개선

--- example/Source10_13.cs
+++ example/Source10_13.cs
@@ -1,1 +1,11 @@
-using System;class MainClass {  public static void Main(string[] args) {    var variable = "Hello";    Console.WriteLine(variable);        variable = 123;    Console.WriteLine(variable);  }}
+using System;
+
+class MainClass {
+  public static void Main(string[] args) {
+    var variable = "Hello";
+    Console.WriteLine(variable);
+    
+    variable = 123;
+    Console.WriteLine(variable);
+  }
+}
