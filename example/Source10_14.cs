e52d50f35188620b5213e4e57f5151e521a344a6
yulian <yulian@naver.com>
2022-10-14 02:25:09 +0900

    Update Source10_14.cs
    코드 가독성 개선

--- example/Source10_14.cs
+++ example/Source10_14.cs
@@ -1,1 +1,11 @@
-using System;class MainClass {  public static void Main(string[] args) {    dynamic variable = "Hello";    Console.WriteLine(variable);        variable = 123;    Console.WriteLine(variable);  }}
+using System;
+
+class MainClass {
+  public static void Main(string[] args) {
+    dynamic variable = "Hello";
+    Console.WriteLine(variable);
+    
+    variable = 123;
+    Console.WriteLine(variable);
+  }
+}
