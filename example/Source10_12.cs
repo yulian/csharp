9e9167da6c779ff729b289a3682bb2004d2f645f
yulian <yulian@naver.com>
2022-10-14 02:24:42 +0900

    Update Source10_12.cs
    코드 가독성 개선

--- example/Source10_12.cs
+++ example/Source10_12.cs
@@ -1,1 +1,15 @@
-using System;using System.Collections;class MainClass {  public static void Main(string[] args) {    ArrayList al = new ArrayList();    al.Add(1);    al.Add("Hi");    al.Add(true);        foreach(var item in al) {      Console.WriteLine(item);    }  }}
+using System;
+using System.Collections;
+
+class MainClass {
+  public static void Main(string[] args) {
+    ArrayList al = new ArrayList();
+    al.Add(1);
+    al.Add("Hi");
+    al.Add(true);
+    
+    foreach(var item in al) {
+      Console.WriteLine(item);
+    }
+  }
+}
