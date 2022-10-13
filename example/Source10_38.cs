51edc9b3b96a164c27d07fe5ede7659a906b3f76
yulian <yulian@naver.com>
2022-10-14 02:31:18 +0900

    Update Source10_38.cs
    코드 가독성 개선

--- example/Source10_38.cs
+++ example/Source10_38.cs
@@ -1,1 +1,23 @@
-using System;class MainClass {  public static void Main(string[] args) {    var fruits = new string[] {                // 시작 인덱스  끝 인덱스      "apple",  // 0            ^4      "banana", // 1            ^3      "orange", // 2            ^2      "cherry", // 3            ^1    };        Console.WriteLine(fruits[0]);    Console.WriteLine(fruits[1]);    Console.WriteLine(fruits[2]);    Console.WriteLine(fruits[3]);        Console.WriteLine(fruits[^1]);    Console.WriteLine(fruits[^2]);    Console.WriteLine(fruits[^3]);    Console.WriteLine(fruits[^4]);  }}
+using System;
+
+class MainClass {
+  public static void Main(string[] args) {
+    var fruits = new string[] {
+                // 시작 인덱스  끝 인덱스
+      "apple",  // 0            ^4
+      "banana", // 1            ^3
+      "orange", // 2            ^2
+      "cherry", // 3            ^1
+    };
+    
+    Console.WriteLine(fruits[0]);
+    Console.WriteLine(fruits[1]);
+    Console.WriteLine(fruits[2]);
+    Console.WriteLine(fruits[3]);
+    
+    Console.WriteLine(fruits[^1]);
+    Console.WriteLine(fruits[^2]);
+    Console.WriteLine(fruits[^3]);
+    Console.WriteLine(fruits[^4]);
+  }
+}
