b0e08f5d6bee0b0b8cd639debf8a6f7f85aacc12
yulian <yulian@naver.com>
2022-10-14 02:27:40 +0900

    Update Source10_23.cs
    코드 가독성 개선

--- example/Source10_23.cs
+++ example/Source10_23.cs
@@ -1,1 +1,35 @@
-using System;public class Circle {  public Circle(double radius) {    Radius = radius;  }    public double Radius { get; set; }  public double Diameter {    get {      return 2 * Radius;    }  }    public double Circumference {    get {      return 2 * Radius * Math.PI;    }  }    public double Area {    get {      return Math.PI * Math.Pow(Radius, 2);    }  }}class MainClass {  public static void Main(string[] args) {    Circle c = new Circle(3);    Console.WriteLine(c.Diameter);    Console.WriteLine(c.Circumference);    Console.WriteLine(c.Area);  }}
+using System;
+
+public class Circle {
+  public Circle(double radius) {
+    Radius = radius;
+  }
+  
+  public double Radius { get; set; }
+  public double Diameter {
+    get {
+      return 2 * Radius;
+    }
+  }
+  
+  public double Circumference {
+    get {
+      return 2 * Radius * Math.PI;
+    }
+  }
+  
+  public double Area {
+    get {
+      return Math.PI * Math.Pow(Radius, 2);
+    }
+  }
+}
+
+class MainClass {
+  public static void Main(string[] args) {
+    Circle c = new Circle(3);
+    Console.WriteLine(c.Diameter);
+    Console.WriteLine(c.Circumference);
+    Console.WriteLine(c.Area);
+  }
+}
