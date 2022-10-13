58e85f9c5dea8c456fa4ccf0c6a5587a3b4425a2
yulian <yulian@naver.com>
2022-10-14 02:30:07 +0900

    Update Source10_33.cs
    코드 가독성 개선

--- example/Source10_33.cs
+++ example/Source10_33.cs
@@ -1,1 +1,11 @@
-using System;class MainClass {  public static void Main(string[] args) {    (double, int) t1 = (4.5, 3);    Console.WriteLine($"튜플 내 항목은 {t1.Item1}과 {t1.Item2}입니다.");        (double Sum, int Count) t2 = (4.5, 3);    Console.WriteLine($"Count: {t2.Count}, Sum: {t2.Sum}");  }}
+using System;
+
+class MainClass {
+  public static void Main(string[] args) {
+    (double, int) t1 = (4.5, 3);
+    Console.WriteLine($"튜플 내 항목은 {t1.Item1}과 {t1.Item2}입니다.");
+    
+    (double Sum, int Count) t2 = (4.5, 3);
+    Console.WriteLine($"Count: {t2.Count}, Sum: {t2.Sum}");
+  }
+}
