7e453436c4cfdfb8da7a582124bf4b864d2db3ff
yulian <yulian@naver.com>
2022-10-14 01:58:10 +0900

    Update Source08_09.cs
    코드 가독성 개선

--- example/Source08_09.cs
+++ example/Source08_09.cs
@@ -1,1 +1,17 @@
-using System.Collections;using System.Collections.Generic;using UnityEngine;public class AttackAnimation: MonoBehaviour {  public Animator character;    void Start() {    character = GetComponent < Animator > ();  }    void Update() {    if (Input.GetKeyDown(KeyCode.Space)) {      character.SetTrigger(“ATTACK”);    }  }}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAnimation: MonoBehaviour {
+  public Animator character;
+  
+  void Start() {
+    character = GetComponent < Animator > ();
+  }
+  
+  void Update() {
+    if (Input.GetKeyDown(KeyCode.Space)) {
+      character.SetTrigger(“ATTACK”);
+    }
+  }
+}
