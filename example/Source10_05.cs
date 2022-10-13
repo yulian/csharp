4cf91bff0a142491769f6b3cd5beb70e3623d1e6
yulian <yulian@naver.com>
2022-10-14 02:22:45 +0900

    Update Source10_05.cs
    코드 가독성 개선

--- example/Source10_05.cs
+++ example/Source10_05.cs
@@ -1,1 +1,14 @@
-using System;class MainClass {  public static void Main(string[] args) {    int ? var1 = 10;    int ? var2 = null;        Console.WriteLine(var1 + var2);    Console.WriteLine(var1 * var2);    Console.WriteLine(var1 == var2);    Console.WriteLine(var1 > var2);    Console.WriteLine(var2 == var2);  }}
+using System;
+
+class MainClass {
+  public static void Main(string[] args) {
+    int ? var1 = 10;
+    int ? var2 = null;
+    
+    Console.WriteLine(var1 + var2);
+    Console.WriteLine(var1 * var2);
+    Console.WriteLine(var1 == var2);
+    Console.WriteLine(var1 > var2);
+    Console.WriteLine(var2 == var2);
+  }
+}
