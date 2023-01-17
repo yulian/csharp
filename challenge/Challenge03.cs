ef24a04091300f561f7303478719eb198adda9f7
yulian <yulian@naver.com>
2023-01-17 15:43:20 +0900

    Create Challenge03.cs

---
+++ challenge/Challenge03.cs
@@ -0,0 +1,9 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int celsius = 27;
+    double fahrenheit = (celsius*1.8)+32;
+    Console.WriteLine("섭씨 온도 "+celsius+"도는 화씨 온도 "+fahrenheit+"입니다.");
+  }
+}
