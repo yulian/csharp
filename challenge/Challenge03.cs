fee518c24843299946f9b17a12402463a6e15ff0
yulian <yulian@naver.com>
2023-01-17 15:59:39 +0900

    Update Challenge03.cs

--- challenge/Challenge03.cs
+++ challenge/Challenge03.cs
@@ -4,8 +4,13 @@ class MainClass
 {
   public static void Main (string[] args)
   {
+    // 섭씨 온도 초기화
     int celsius = 27;
+    
+    // 섭씨 온도를 화씨 온도로 변환
     double fahrenheit = (celsius*1.8)+32;
+    
+    // 변환된 화씨 온도값 출력
     Console.WriteLine("섭씨 온도 "+celsius+"도는 화씨 온도 "+fahrenheit+"입니다.");
   }
 }
